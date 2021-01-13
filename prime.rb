b7b77669f8f39efd265ce2c19dda33bbd1ad4220
lukevangorder <lukevangorder@gmail.com>
2021-01-13 17:02:57 -0500

    commit tree 33141c8932bf679801bde8e30aaaad015bb8493e

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+require 'pry'
+def prime?(int)
+    if int<=1
+        false
+    else
+        i=0
+        checking = (2..int-1).to_a
+        while i<checking.length
+            denom=checking[i]
+            checking[i]=int%denom
+            i+=1
+        end
+        if checking.include?(0)
+            return false
+        else
+            return true
+        end
+    end
+end
+prime?(10)
+       
